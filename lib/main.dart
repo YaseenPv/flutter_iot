9562f4ef648a1b7ae921c06545311692db8b6855
Muhammed <Yaseen@INAXUS.com>
2023-10-13 16:30:13 +0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iot/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          fontFamily: "Poppins",
+          sliderTheme: const SliderThemeData(
+            trackShape: RectangularSliderTrackShape(),
+            trackHeight: 2.5,
+            thumbShape: RoundSliderThumbShape(enabledThumbRadius: 8.0),
+            overlayShape: RoundSliderOverlayShape(overlayRadius: 15.0),
+          )),
+      home: const MyHomePage(),
+    );
+  }
+}
+
